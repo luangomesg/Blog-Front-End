545307dceaa107b706fb852fbb5423a4d2d66f96
Luan Gomes Galvão <luanitachi@hotmail.com>
2023-03-26 18:36:26 -0300

    finish

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Home from './src/pages/Home/index';
-import GlobalStyles from './src/globalStyles';
+import Home from './pages/Home/index';
+import GlobalStyles from './globalStyles';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
